432f92781496b1d916685da550ae2d4d7d586949
manish kumar <manish8755026341@gmail.com>
2025-09-13 01:04:01 +0530

    server.js update

--- Server/Server.js
+++ Server/Server.js
@@ -56,7 +56,16 @@ app.use("/api/messages", messageRouter);
 //contect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+
+if(process.env.NODE_ENV === "production") {
+ 
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+//export server for verscel
+export default server;
+
